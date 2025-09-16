dad17823c8ee0287af70ff2b6db814c3aed88a14
Anas Temouden <temo27anas@gmail.com>
2025-09-16 09:34:42 +0300

    java t2: implement & test executeDueTasks & scheduleTask

--- java_task/task2/TaskScheduler.java
+++ java_task/task2/TaskScheduler.java
@@ -9,15 +9,54 @@ public class TaskScheduler {
     // Priority queue to hold tasks
     private final PriorityQueue<Task> queue = new PriorityQueue<>();
 
+   // schedules a new task 
     public void scheduleTask(String id, long epochSeconds, String description) {
+        Task newTask = new Task(id, epochSeconds, description);
+        queue.add(newTask);
     }
 
-    public void executeNextTask() {
+    // executes the next task in the queue
+    public void executeDueTasks() {
+        long currentTime = System.currentTimeMillis() / 1000;
+        Task nextTask = queue.peek(); // select the task with the earliest time
+        
+
+        while (nextTask != null
+                && nextTask.compareTo(new Task("", currentTime, "")) <= 0) {
+            System.out.println("polling task: " + nextTask.getDescription());
+            queue.poll(); // remove the executed task from the queue
+            nextTask = queue.peek(); // get the next task
+        }
+
+
+
     }
 
+
+    public static void main(String[] args) {
+
+        TaskScheduler scheduler = new TaskScheduler();
+        long now = java.time.Instant.now().getEpochSecond();
+
+        scheduler.scheduleTask("t1", now + 2, "Backup database");
+        scheduler.scheduleTask("t2", now + 2, "Send report email 1");
+        scheduler.scheduleTask("t2", now + 2, "Send report email 2");
+        scheduler.scheduleTask("t2", now + 2, "Send report email 3");
+        scheduler.scheduleTask("late", now - 5, "Run missed task");
+
+        // simple loop
+        for (int i = 0; i < 6; i++) {
+        scheduler.executeDueTasks();
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                Thread.currentThread().interrupt();
+            }
+        }
+
+    }
 }
-
-
 
 class Task implements Comparable<Task> {
     /*
@@ -34,9 +73,8 @@ class Task implements Comparable<Task> {
         this.description = description;
     }
 
-    @Override
-    public String toString() {
-        return "name; " + name + " - epochSeconds = " + epochSeconds + "- description = " + description;
+    public String getDescription() {
+        return description;
     }
 
     @Override
